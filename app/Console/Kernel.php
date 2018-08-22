e996c165055b74d45b120aaa739deb92911dde3c
Kovah <mail@kovah.de>
2018-08-22 23:08:51 +0200

    Add a command to register a new user from the CLI

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RegisterUser;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        RegisterUser::class
     ];
 
     /**
---
+++ app/Console/RegisterUser.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * Class RegisterUser
+ *
+ * Provides a php artisan command to register a new user
+ * php artisan registeruser UserName mail@user.com mysecretpassword []
+ *
+ * @package App\Console\Commands
+ */
+class RegisterUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'registeruser {name : Username} {email : User email address} {password : User password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Register a new user';
+
+    /**
+     * RegisterUser constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+
+        if (empty($name)) {
+            $this->warn('Name is missing!');
+            return;
+        }
+
+        if (empty($email)) {
+            $this->warn('Email address is missing!');
+            return;
+        }
+
+        if (empty($password)) {
+            $this->warn('Password is missing!');
+            return;
+        }
+
+        // Check if the user exists
+        if (User::where('email', $email)->first()) {
+            $this->error('An user with the email address "' . $email . '" already exists!');
+            return;
+        }
+
+        $new_user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info('User ' . $name . ' registered.');
+    }
+}
