76795da0c2bb0e8536a8cf79c41d312200c73014
Kovah <mail@kovah.de>
2018-08-27 13:02:30 +0200

    Correct the navigation

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="{{ route('front') }}">
+        <a class="navbar-item" href="{{ auth()->guest() ? route('front') : route('home') }}">
             <img src="{{ asset('assets/img/logo_linkace.svg') }}" alt="@lang('linkace.linkace')"
                 width="76" height="28">
         </a>
