54c3311ac5e3d3ab69546f3a13bbe218ba96a470
Kovah <mail@kovah.de>
2018-09-11 00:00:07 +0200

    Center pagination

--- resources/views/partials/card-pagination.blade.php
+++ resources/views/partials/card-pagination.blade.php
@@ -1,5 +1,5 @@
 @if ($paginator->hasPages())
-    <ul class="pagination" role="navigation">
+    <ul class="pagination justify-content-center" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
