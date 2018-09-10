887e3a46a3161361af86e7f4cdc032baa528ccd8
Kovah <mail@kovah.de>
2018-09-10 23:18:12 +0200

    BS4: Login and navbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,70 +2,82 @@
 
 @section('content')
 
-    <div class="columns is-centered">
-        <div class="column is-half">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8">
 
             <div class="card">
-                <div class="card-content">
-
+                <div class="card-header">
+                    @lang('linkace.login')
+                </div>
+                <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" aria-label="@lang('linkace.login')">
                         @csrf
 
-                        <div class="field">
-                            <label class="label" for="email">@lang('linkace.email')</label>
-                            <div class="control">
-                                <input name="email" id="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}"
-                                    type="email" placeholder="@lang('linkace.email')" value="{{ old('email') }}"
-                                    required autofocus>
+                        <div class="form-group">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">
+                                        <i class="fa fa-fw fa-envelope"></i>
+                                    </div>
+                                </div>
+                                <input type="email" name="email" id="email" class="form-control"
+                                    value="{{ old('email') }}"
+                                    placeholder="@lang('linkace.email')" aria-label="@lang('linkace.email')" required
+                                    autofocus>
                             </div>
+
                             @if ($errors->has('email'))
-                                <p class="help has-text-danger" role="alert">
+                                <p class="help small text-danger" role="alert">
                                     {{ $errors->first('email') }}
                                 </p>
                             @endif
                         </div>
 
-                        <div class="field">
-                            <label class="label" for="password">@lang('linkace.password')</label>
-                            <div class="control">
-                                <input name="password" id="password"
-                                    class="input{{ $errors->has('password') ? ' is-danger' : '' }}"
-                                    type="password" placeholder="@lang('linkace.password')" required>
+                        <div class="form-group">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">
+                                        <i class="fa fa-fw fa-lock"></i>
+                                    </div>
+                                </div>
+                                <input type="password" name="password" id="password" class="form-control"
+                                    placeholder="@lang('linkace.password')" aria-label="@lang('linkace.password')">
                             </div>
                             @if ($errors->has('password'))
-                                <p class="help has-text-danger" role="alert">
+                                <p class="help small text-danger" role="alert">
                                     {{ $errors->first('password') }}
                                 </p>
                             @endif
                         </div>
 
-                        <div class="field">
-                            <div class="control">
-                                <label class="checkbox">
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                    @lang('linkace.remember_me')
-                                </label>
+                        <div class="row mt-4">
+                            <div class="col-8">
+
+                                <div class="custom-control custom-checkbox pt-1">
+                                    <input type="hidden" name="remember_me" value="0">
+                                    <input type="checkbox" class="custom-control-input" id="remember_me"
+                                        @if(old('remember_me')) checked @endif>
+
+                                    <label class="custom-control-label" for="remember_me">
+                                        @lang('linkace.remember_me')
+                                    </label>
+                                </div>
+
                             </div>
-                        </div>
+                            <div class="col-4">
 
-                        <br>
-
-                        <div class="field is-grouped">
-                            <div class="control">
-                                <button type="submit" class="button is-primary">
-                                    <i class="fa fa-unlock fa-mr"></i> @lang('linkace.login')
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    @lang('linkace.login')
                                 </button>
+
                             </div>
                         </div>
 
                     </form>
-
                 </div>
             </div>
 
-            <br>
-
-            <small class="has-text-grey">
+            <small class="text-muted mt-3">
                 {!! trans('linkace.forgot_password_link', ['reset_url' => route('password.request')]) !!}
             </small>
 
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,80 +1,87 @@
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar navbar-expand-md navbar-light shadow-sm">
     <div class="container">
+        <a class="navbar-brand" href="{{ auth()->guest() ? route('front') : route('dashboard') }}">
+            <img src="{{ asset('assets/img/logo_linkace.svg') }}" alt="@lang('linkace.linkace')"
+                width="81" height="30">
+        </a>
 
-        <div class="navbar-brand">
-            <a class="navbar-item" href="{{ auth()->guest() ? route('front') : route('dashboard') }}">
-                <img src="{{ asset('assets/img/logo_linkace.svg') }}" alt="@lang('linkace.linkace')"
-                    width="76" height="28">
-            </a>
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            @auth
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar-links-dd" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            @lang('link.links')
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbar-links-dd">
+                            <a href="{{ route('links.index') }}" class="dropdown-item">
+                                @lang('link.all_links')
+                            </a>
+                            <a href="{{ route('links.create') }}" class="dropdown-item">
+                                @lang('link.add')
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar-categories-dd" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            @lang('category.categories')
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbar-categories-dd">
+                            <a href="{{ route('categories.index') }}" class="dropdown-item">
+                                @lang('category.all_categories')
+                            </a>
+                            <a href="{{ route('categories.create') }}" class="dropdown-item">
+                                @lang('category.add')
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar-tags-dd" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            @lang('tag.tags')
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbar-tags-dd">
+                            <a href="{{ route('tags.index') }}" class="dropdown-item">
+                                @lang('tag.all_tags')
+                            </a>
+                            <a href="{{ route('tags.create') }}" class="dropdown-item">
+                                @lang('tag.add')
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            @endauth
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">
+                            @lang('linkace.login')
+                        </a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="{{ route('get-search') }}" class="nav-link">
+                            @lang('search.search')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            @lang('linkace.logout')
+                        </a>
+                    </li>
+                @endguest
+            </ul>
         </div>
-
-        <div class="navbar-menu">
-            @guest
-                <a href="{{ route('login') }}" class="navbar-item">
-                    @lang('linkace.login')
-                </a>
-            @else
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        @lang('link.links')
-                    </a>
-                    <div class="navbar-dropdown">
-                        <a href="{{ route('links.index') }}" class="navbar-item">
-                            @lang('link.all_links')
-                        </a>
-                        <a href="{{ route('links.create') }}" class="navbar-item">
-                            @lang('link.add')
-                        </a>
-                    </div>
-                </div>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        @lang('category.categories')
-                    </a>
-                    <div class="navbar-dropdown">
-                        <a href="{{ route('categories.index') }}" class="navbar-item">
-                            @lang('category.all_categories')
-                        </a>
-                        <a href="{{ route('categories.create') }}" class="navbar-item">
-                            @lang('category.add')
-                        </a>
-                    </div>
-                </div>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        @lang('tag.tags')
-                    </a>
-                    <div class="navbar-dropdown">
-                        <a href="{{ route('tags.index') }}" class="navbar-item">
-                            @lang('tag.all_tags')
-                        </a>
-                        <a href="{{ route('tags.create') }}" class="navbar-item">
-                            @lang('tag.add')
-                        </a>
-                    </div>
-                </div>
-
-                <a href="{{ route('get-search') }}" class="navbar-item ml-auto">
-                    @lang('search.search')
-                </a>
-
-                <a onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-                    class="navbar-item">
-                    @lang('linkace.logout')
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            @endguest
-        </div>
-
     </div>
 </nav>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
