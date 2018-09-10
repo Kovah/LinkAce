51b685362393ae36a320eaee2d2d849f70ec78d3
Kovah <mail@kovah.de>
2018-09-10 23:30:57 +0200

    BS4: Partials

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -1,11 +1,11 @@
 @if (session()->has('alert.message'))
-    <div class="notification is-{{ session()->get('alert.style') }}" role="alert">
+    <div class="alert alert-{{ session()->get('alert.style') }}" role="alert">
         {!! session()->get('alert.message') !!}
     </div><!-- /alert -->
 @endif
 
 @if ($errors->any())
-    <div class="notification is-danger">
+    <div class="alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
--- resources/views/partials/card-pagination.blade.php
+++ resources/views/partials/card-pagination.blade.php
@@ -1,36 +1,30 @@
 @if ($paginator->hasPages())
-    <div class="card-footer" role="navigation">
+    <ul class="pagination" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <div class="card-footer-item is-disabled" aria-disabled="true"
-                aria-label="@lang('pagination.previous')">
-                <span aria-hidden="true">&lsaquo;</span>
-            </div>
+            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                <span class="page-link" aria-hidden="true">&lsaquo;</span>
+            </li>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="card-footer-item"
-                aria-label="@lang('pagination.previous')">
-                &lsaquo;
-            </a>
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+            </li>
         @endif
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <div class="card-footer-item is-disabled" aria-disabled="true"><span>{{ $element }}</span></div>
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <div class="card-footer-item is-active" aria-current="page">
-                            <span>{{ $page }}</span>
-                        </div>
+                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                     @else
-                        <a href="{{ $url }}" class="card-footer-item">
-                            {{ $page }}
-                        </a>
+                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
@@ -38,16 +32,13 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="card-footer-item" rel="next"
-                aria-label="@lang('pagination.next')">
-                &rsaquo;
-            </a>
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+            </li>
         @else
-            <div class="card-footer-item is-disabled" aria-disabled="true"
-                aria-label="@lang('pagination.next')">
-                <span aria-hidden="true">&rsaquo;</span>
-            </div>
+            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                <span class="page-link" aria-hidden="true">&rsaquo;</span>
+            </li>
         @endif
-    </div>
+    </ul>
 @endif
-
