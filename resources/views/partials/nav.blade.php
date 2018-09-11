1881a2a407d01fe6c63be522fd2940e4f5814e45
Kovah <mail@kovah.de>
2018-09-11 23:04:13 +0200

    Fix navbar displaying on mobile

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,12 +5,12 @@
                 width="81" height="30">
         </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content"
+            aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbar-content">
             @auth
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown">
