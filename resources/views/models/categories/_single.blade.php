4c8369f91426e3c372824583b871bee9705b6a7d
Kovah <mail@kovah.de>
2018-09-12 08:33:05 +0200

    BS4: Category index

--- resources/views/models/categories/_single.blade.php
+++ resources/views/models/categories/_single.blade.php
@@ -1,33 +1,27 @@
 <tr>
     <td>
         @if(isset($is_child))
-            {!! '<small>' !!}
             &rightarrow;
         @endif
         <a href="{{ route('categories.show', [$category->id]) }}">
             {{ $category->name }}
         </a>
-        @if(isset($is_child)) {!! '</small>' !!} @endif
     </td>
     <td>
         <small>{{ $category->description }}</small>
     </td>
-    <td class="has-text-right">
+    <td class="text-right">
         <small>{{ $category->links->count() }}</small>
     </td>
     <td>
-        <div class="field has-addons">
-            <div class="control">
-                <a href="{{ route('categories.edit', [$category->id]) }}" class="button is-small">
-                    <i class="fa fa-pencil"></i>
-                </a>
-            </div>
-            <div class="control">
-                <a onclick="event.preventDefault();document.getElementById('category-delete-{{ $category->id }}').submit();"
-                    title=" @lang('category.delete')" class="button is-small is-danger">
-                    <i class="fa fa-trash"></i>
-                </a>
-            </div>
+        <div class="btn-group btn-group-sm">
+            <a href="{{ route('categories.edit', [$category->id]) }}" class="btn  btn-outline-primary">
+                <i class="fa fa-pencil"></i>
+            </a>
+            <a onclick="event.preventDefault();document.getElementById('category-delete-{{ $category->id }}').submit();"
+                title=" @lang('category.delete')" class="btn btn-outline-danger">
+                <i class="fa fa-trash"></i>
+            </a>
         </div>
         <form id="category-delete-{{ $category->id }}" method="POST" style="display: none;"
             action="{{ route('categories.destroy', [$category->id]) }}">
--- resources/views/models/categories/_table.blade.php
+++ resources/views/models/categories/_table.blade.php
@@ -1,20 +1,22 @@
-<table class="table">
-    <thead>
-    <tr>
-        <th>@lang('category.name')</th>
-        <th>@lang('category.description')</th>
-        <th class="has-text-right">@lang('link.links')</th>
-        <th></th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($categories as $category)
-        @include('models.categories._single', ['category' => $category])
-        @if($category->childCategories)
-            @foreach($category->childCategories as $child_category)
-                @include('models.categories._single', ['category' => $child_category, 'is_child' => true])
-            @endforeach
-        @endif
-    @endforeach
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-bordered table-hover mb-0">
+        <thead>
+        <tr>
+            <th>@lang('category.name')</th>
+            <th>@lang('category.description')</th>
+            <th class="text-right">@lang('link.links')</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($categories as $category)
+            @include('models.categories._single', ['category' => $category])
+            @if($category->childCategories)
+                @foreach($category->childCategories as $child_category)
+                    @include('models.categories._single', ['category' => $child_category, 'is_child' => true])
+                @endforeach
+            @endif
+        @endforeach
+        </tbody>
+    </table>
+</div>
