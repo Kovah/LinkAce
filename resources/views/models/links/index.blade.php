654db3733809562f9d16d508075c60fa735a784b
Kovah <mail@kovah.de>
2018-09-12 08:23:26 +0200

    Update HTML semantics

--- resources/views/models/links/index.blade.php
+++ resources/views/models/links/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="d-flex align-items-center justify-content-between">
+    <header class="d-flex align-items-center justify-content-between">
         <h3 class="mb-0">
             @lang('link.links')
         </h3>
@@ -10,9 +10,9 @@
             <i class="fa fa-plus fa-mr"></i>
             @lang('linkace.add')
         </a>
-    </div>
+    </header>
 
-    <div class="link-wrapper my-3">
+    <section class="link-wrapper my-3">
         @if(!$links->isEmpty())
 
             @foreach($links as $link)
@@ -21,12 +21,12 @@
 
         @else
 
-            <div class="alert alert-warning">
+            <div class="alert alert-info">
                 @lang('linkace.no_results_found', ['model' => trans('link.links')])
             </div>
 
         @endif
-    </div>
+    </section>
 
     @if(!$links->isEmpty())
         {!! $links->links('partials.card-pagination', ['paginator' => $links]) !!}
