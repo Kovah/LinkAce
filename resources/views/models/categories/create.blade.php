802c17d860a0da0d96f6bce69ec56c68d629b728
Kovah <mail@kovah.de>
2018-09-14 11:32:04 +0200

    BS4: Category create view

--- resources/views/models/categories/create.blade.php
+++ resources/views/models/categories/create.blade.php
@@ -4,24 +4,23 @@
 
     <div class="card">
         <header class="card-header">
-            <p class="card-header-title">
-                @lang('category.add')
-            </p>
+            @lang('category.add')
         </header>
-        <div class="card-content">
+        <div class="card-body">
 
             <form action="{{ route('categories.store') }}" method="POST">
                 @csrf
 
-                <div class="field">
-                    <label class="label" for="name">@lang('category.name')</label>
-                    <div class="control">
-                        <input name="name" id="name" class="input is-large{{ $errors->has('name') ? ' is-danger' : '' }}"
-                            type="text" placeholder="@lang('category.name')" value="{{ old('name') }}"
-                            required autofocus>
-                    </div>
+                <div class="form-group">
+                    <label for="name">@lang('category.name')</label>
+
+                    <input name="name" id="name"
+                        class="form-control form-control-lg{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                        type="text" placeholder="@lang('category.name')" value="{{ old('name') }}"
+                        required autofocus>
+
                     @if ($errors->has('name'))
-                        <p class="help has-text-danger" role="alert">
+                        <p class="invalid-feedback" role="alert">
                             {{ $errors->first('name') }}
                         </p>
                     @endif
@@ -29,58 +28,63 @@
 
                 <br>
 
-                <div class="columns">
-                    <div class="column is-half">
+                <div class="row">
+                    <div class="col">
 
-                        <div class="field">
-                            <label class="label" for="description">@lang('category.description')</label>
-                            <div class="control">
-                                <textarea name="description" id="description" rows="4" class="textarea"
-                                    placeholder="@lang('category.description')">{{ old('description') }}</textarea>
-                            </div>
+                        <div class="form-group">
+                            <label for="description">
+                                @lang('category.description')
+                            </label>
+
+                            <textarea name="description" id="description" rows="4" class="form-control"
+                                placeholder="@lang('category.description')">{{ old('description') }}</textarea>
+
                             @if ($errors->has('description'))
-                                <p class="help has-text-danger" role="alert">
+                                <p class="invalid-feedback" role="alert">
                                     {{ $errors->first('description') }}
                                 </p>
                             @endif
                         </div>
 
                     </div>
-                    <div class="column is-half">
+                    <div class="col">
 
-                        <div class="field">
-                            <label class="label" for="parent_category">@lang('category.parent_category')</label>
-                            <div class="control">
-                                <div class="select{{ $errors->has('parent_category') ? ' is-danger' : '' }}">
-                                    <select id="parent_category" name="parent_category">
-                                        <option value="0">@lang('category.select_parent_category')</option>
-                                        @foreach($categories as $category)
-                                            <option value="{{ $category->id }}">
-                                                {{ $category->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
+                        <div class="form-group">
+                            <label for="parent_category">
+                                @lang('category.parent_category')
+                            </label>
+
+                            <select id="parent_category" name="parent_category"
+                                class="form-control{{ $errors->has('parent_category') ? ' is-invalid' : '' }}">
+                                <option value="0">@lang('category.select_parent_category')</option>
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}">
+                                        {{ $category->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+
+
                             @if ($errors->has('parent_category'))
-                                <p class="help has-text-danger" role="alert">
+                                <p class="invalid-feedback" role="alert">
                                     {{ $errors->first('parent_category') }}
                                 </p>
                             @endif
                         </div>
 
-                        <div class="field">
-                            <label class="label" for="is_private">@lang('linkace.is_private')</label>
-                            <div class="control">
-                                <div class="select{{ $errors->has('is_private') ? ' is-danger' : '' }}">
-                                    <select id="is_private" name="is_private">
-                                        <option value="0">@lang('linkace.no')</option>
-                                        <option value="1">@lang('linkace.yes')</option>
-                                    </select>
-                                </div>
-                            </div>
+                        <div class="form-group">
+                            <label for="is_private">
+                                @lang('linkace.is_private')
+                            </label>
+
+                            <select id="is_private" name="is_private"
+                                class="form-control{{ $errors->has('parent_category') ? ' is-invalid' : '' }}">
+                                <option value="0">@lang('linkace.no')</option>
+                                <option value="1">@lang('linkace.yes')</option>
+                            </select>
+
                             @if ($errors->has('is_private'))
-                                <p class="help has-text-danger" role="alert">
+                                <p class="invalid-feedback" role="alert">
                                     {{ $errors->first('is_private') }}
                                 </p>
                             @endif
@@ -89,22 +93,20 @@
                     </div>
                 </div>
 
-                <br>
+                <div class="mt-3 d-flex align-items-center">
 
-                <div class="field">
-                    <div class="control is-flex align-items-center has-text-right">
-
-                        <label class="checkbox mr ml-auto has-text-grey-light">
-                            <input type="checkbox" name="reload_view"
-                                @if(session('reload_view')) checked @endif>
+                    <div class="custom-control custom-checkbox ml-auto mr-4">
+                        <input class="custom-control-input" type="checkbox" id="reload_view" name="reload_view"
+                            @if(session('reload_view')) checked @endif>
+                        <label class="custom-control-label" for="reload_view">
                             @lang('linkace.continue_adding')
                         </label>
-
-                        <button type="submit" class="button is-primary is-medium">
-                            <i class="fa fa-save fa-mr"></i> @lang('category.add')
-                        </button>
-
                     </div>
+
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-save fa-mr"></i> @lang('category.add')
+                    </button>
+
                 </div>
 
             </form>
