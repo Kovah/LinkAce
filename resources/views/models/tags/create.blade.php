b5ca0466b6282997bb29ff9d7b963642c02acc44
Kovah <mail@kovah.de>
2018-09-14 11:43:44 +0200

    BS4: Tag create view

--- resources/views/models/tags/create.blade.php
+++ resources/views/models/tags/create.blade.php
@@ -4,65 +4,63 @@
 
     <div class="card">
         <header class="card-header">
-            <p class="card-header-title">
-                @lang('tag.add')
-            </p>
+            @lang('tag.add')
         </header>
-        <div class="card-content">
+        <div class="card-body">
 
             <form action="{{ route('tags.store') }}" method="POST">
                 @csrf
 
-                <div class="field">
-                    <label class="label" for="name">@lang('tag.name')</label>
-                    <div class="control">
-                        <input name="name" id="name"
-                            class="input is-large{{ $errors->has('name') ? ' is-danger' : '' }}"
-                            type="text" placeholder="@lang('tag.name')" value="{{ old('name') }}"
-                            required autofocus>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="name">@lang('tag.name')</label>
+
+                            <input name="name" id="name"
+                                class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                type="text" placeholder="@lang('tag.name')" value="{{ old('name') }}"
+                                required autofocus>
+
+                            @if ($errors->has('name'))
+                                <p class="invalid-feedback" role="alert">
+                                    {{ $errors->first('name') }}
+                                </p>
+                            @endif
+                        </div>
                     </div>
-                    @if ($errors->has('name'))
-                        <p class="help has-text-danger" role="alert">
-                            {{ $errors->first('name') }}
-                        </p>
-                    @endif
-                </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="is_private">@lang('linkace.is_private')</label>
 
-                <br>
-
-                <div class="field">
-                    <label class="label" for="is_private">@lang('linkace.is_private')</label>
-                    <div class="control">
-                        <div class="select{{ $errors->has('is_private') ? ' is-danger' : '' }}">
-                            <select id="is_private" name="is_private">
+                            <select id="is_private" name="is_private"
+                                class="custom-select{{ $errors->has('is_private') ? ' is-invalid' : '' }}">
                                 <option value="0">@lang('linkace.no')</option>
                                 <option value="1">@lang('linkace.yes')</option>
                             </select>
+
+                            @if ($errors->has('is_private'))
+                                <p class="invalid-feedback" role="alert">
+                                    {{ $errors->first('is_private') }}
+                                </p>
+                            @endif
                         </div>
                     </div>
-                    @if ($errors->has('is_private'))
-                        <p class="help has-text-danger" role="alert">
-                            {{ $errors->first('is_private') }}
-                        </p>
-                    @endif
                 </div>
 
-                <br>
+                <div class="mt-3 d-flex align-items-center">
 
-                <div class="field">
-                    <div class="control is-flex align-items-center has-text-right">
-
-                        <label class="checkbox mr ml-auto has-text-grey-light">
-                            <input type="checkbox" name="reload_view"
-                                @if(session('reload_view')) checked @endif>
+                    <div class="custom-control custom-checkbox ml-auto mr-4">
+                        <input class="custom-control-input" type="checkbox" id="reload_view" name="reload_view"
+                            @if(session('reload_view')) checked @endif>
+                        <label class="custom-control-label" for="reload_view">
                             @lang('linkace.continue_adding')
                         </label>
-
-                        <button type="submit" class="button is-primary is-medium">
-                            <i class="fa fa-save fa-mr"></i> @lang('tag.add')
-                        </button>
-
                     </div>
+
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-save fa-mr"></i> @lang('tag.add')
+                    </button>
+
                 </div>
 
             </form>
