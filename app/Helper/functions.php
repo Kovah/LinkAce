5a9d7a2f02648629c7567a6755ea023922d7452f
Kovah <mail@kovah.de>
2019-01-10 00:02:59 +0100

    Implement helper function for user settings (#2)

---
+++ app/Helper/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Shorthand for the current user settings
+ *
+ * @param string $key
+ * @return mixed
+ */
+function usersettings(string $key = '')
+{
+    if (!auth()->user()) {
+        return null;
+    }
+
+    if (!empty($key)) {
+        return auth()->user()->settings()->get($key);
+    }
+
+    return auth()->user()->settings();
+}
