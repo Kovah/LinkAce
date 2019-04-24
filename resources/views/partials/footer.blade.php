40261eaf096dec59675c02caa4d3941c100123a0
Kovah <mail@kovah.de>
2019-04-24 22:02:10 +0200

    Update footer link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <aside class="footer container text-center small">
-    Linkace is a project by <a href="https://kovah.de/">Kovah.de</a>
+    Linkace is a project by <a href="https://kovah.de/?pk_campaign=opensource&pk_kwd=linkace" rel="noopener" target="_blank">Kovah.de</a>
 </aside>
