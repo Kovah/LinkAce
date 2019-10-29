debed76abd9c7249b7cd5fdd0a83d94d9360ae4a
Kovah <mail@kovah.de>
2019-10-29 16:57:34 +0100

    Add the new list model

---
+++ app/Models/LinkList.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use App\Scopes\OrderNameScope;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class LinkList
+ *
+ * @package  App\Models
+ * @property int                    $id
+ * @property int                    $user_id
+ * @property string                 $name
+ * @property ?string                $description
+ * @property int                    $is_private
+ * @property \Carbon\Carbon|null    $created_at
+ * @property \Carbon\Carbon|null    $updated_at
+ * @property string|null            $deleted_at
+ * @property-read Collection|Link[] $links
+ * @property-read User              $user
+ * @method static Builder|Tag byUser($user_id)
+ */
+class LinkList extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'lists';
+
+    public $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'is_private',
+    ];
+
+    /**
+     * Add the OrderNameScope to the Tag model
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new OrderNameScope());
+    }
+
+    /*
+     | ========================================================================
+     | SCOPES
+     */
+
+    /**
+     * Scope for the user relation
+     *
+     * @param Builder $query
+     * @param int     $user_id
+     * @return mixed
+     */
+    public function scopeByUser($query, $user_id)
+    {
+        return $query->where('user_id', $user_id);
+    }
+
+    /*
+     | ========================================================================
+     | RELATIONSHIPS
+     */
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function links()
+    {
+        return $this->belongsToMany('App\Models\Link', 'link_lists', 'list_id', 'link_id');
+    }
+}
