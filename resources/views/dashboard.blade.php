aa3e11377fddcc45abdd14f83b624b6fab231f60
Kovah <mail@kovah.de>
2020-01-26 16:40:28 +0100

    Optimize the dashboard view by limiting links to one line of text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -50,7 +50,7 @@
 
                 <ul class="list-group list-group-flush">
                     @forelse($recent_links as $link)
-                        <a href="{{ route('links.show', [$link->id]) }}" class="list-group-item list-group-item-action">
+                        <a href="{{ route('links.show', [$link->id]) }}" class="list-group-item list-group-item-action one-line">
                             {!! $link->getIcon('mr-1') !!}
                             {{ $link->title }}
                         </a>
