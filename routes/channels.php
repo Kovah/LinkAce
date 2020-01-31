33f302799ff53e7d30cb76fd3a66bc89870839eb
Kovah <mail@kovah.de>
2020-01-31 12:15:09 +0100

    Remove several unused routes

--- routes/channels.php
+++ routes/channels.php
@@ -10,7 +10,3 @@
 | used to check if an authenticated user can listen to the channel.
 |
 */
-
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
