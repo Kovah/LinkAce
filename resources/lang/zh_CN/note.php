047d27fff53c570a2ccba0af3d7d94975a1f5135
Kevin Woblick <mail@kovah.de>
2020-10-09 12:09:03 +0200

    New translations note.php (Chinese Simplified)

---
+++ resources/lang/zh_CN/note.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'note' => '注释',
+    'notes' => '备注',
+
+    'add' => '添加注释',
+    'show' => '显示备注',
+    'edit' => '编辑注释',
+    'update' => '更新注释',
+    'delete' => '删除注释',
+
+    'private' => '私人注释',
+
+    'note_content' => '备注内容',
+
+    'added_successfully' => '已添加注释。',
+    'updated_successfully' => '已更新注释。',
+    'deleted_successfully' => '已删除注释。',
+    'deletion_error' => '无法删除注释。',
+];
