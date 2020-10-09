c941f1ddcf35d862aa90fabee91de2902b671fff
Kevin Woblick <mail@kovah.de>
2020-10-09 12:05:53 +0200

    New translations tag.php (Swedish)

---
+++ resources/lang/sv_SE/tag.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'tag' => '',
+    'tags' => '',
+    'all_tags' => '',
+
+    'add' => '',
+    'show' => '',
+    'edit' => '',
+    'update' => '',
+    'delete' => '',
+
+    'private' => '',
+
+    'name' => '',
+
+    'author' => '',
+
+    'no_tags' => '',
+
+    'added_successfully' => '',
+    'updated_successfully' => '',
+    'deleted_successfully' => '',
+    'deletion_error' => '',
+];
