6164b42407205e6d782fe1747b0b00e1bc234b35
Kevin Woblick <mail@kovah.de>
2020-10-09 12:09:57 +0200

    New translations trash.php (German)

---
+++ resources/lang/de_DE/trash.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'trash' => 'Papierkorb',
+
+    'deleted_links' => 'Löschte Links',
+    'deleted_lists' => 'Gelöschte Listen',
+    'deleted_tags' => 'Gelöschte Tags',
+    'deleted_notes' => 'Gelöschte Notizen',
+
+    'restore' => 'Eintrag wiederherstellen',
+
+    'clear_trash' => 'Papierkorb leeren',
+    'delete_warning' => 'Dauerhaft gelöschte Einträge können nicht wiederhergestellt werden!',
+    'delete_no_entries' => 'Keine Einträge zu löschen.',
+
+    'delete_success.links' => 'Alle Links dauerhaft gelöscht.',
+    'delete_success.lists' => 'Alle Listen dauerhaft gelöscht.',
+    'delete_success.tags' => 'Alle Tags dauerhaft gelöscht.',
+    'delete_success.notes' => 'Alle Notizen dauerhaft gelöscht.',
+
+    'restore.link' => 'Link wurde aus dem Papierkorb wiederhergestellt.',
+    'restore.list' => 'Liste wurde aus dem Papierkorb wiederhergestellt.',
+    'restore.tag' => 'Tag wurde aus dem Papierkorb wiederhergestellt.',
+    'restore.note' => 'Notiz wurde aus dem Papierkorb wiederhergestellt.',
+
+    'restore.not_found' => 'Das wiederherzustellende Element konnte nicht gefunden werden.',
+    'restore.not_allowed' => 'Sie sind nicht berechtigt, dieses Element wiederherzustellen.',
+
+];
