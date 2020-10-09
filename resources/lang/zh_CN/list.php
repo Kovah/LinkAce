4f435212ddaff4bed729f008a30eb6f04f110756
Kevin Woblick <mail@kovah.de>
2020-10-09 12:08:39 +0200

    New translations list.php (Chinese Simplified)

---
+++ resources/lang/zh_CN/list.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'list' => '列表',
+    'lists' => '列表',
+    'all_lists' => '所有列表',
+
+    'add' => '添加列表',
+    'show' => '显示列表',
+    'edit' => '编辑列表',
+    'update' => '更新列表',
+    'delete' => '删除列表',
+
+    'private' => '私有列表',
+
+    'name' => '列表名称',
+    'description' => '列表描述',
+
+    'author' => '创建者：:user',
+
+    'no_lists' => '列表为空',
+
+    'number_links' => ':number 链接在这个列表中|:number 链接在这个列表',
+
+    'added_successfully' => '已添加列表。',
+    'updated_successfully' => '已更新列表。',
+    'deleted_successfully' => '已删除列表。',
+    'deletion_error' => '无法删除列表。',
+];
