5fecb9478b165db27cd00d7f83c25249100f4791
Kevin Woblick <mail@kovah.de>
2020-10-09 12:11:10 +0200

    New translations import.php (German)

---
+++ resources/lang/de_DE/import.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'import' => 'Import',
+    'start_import' => 'Import starten',
+    'import_running' => 'Import läuft...',
+    'import_file' => 'Datei für den Import',
+
+    'import_help' => 'Sie können hier Ihre vorhandenen Lesezeichen importieren. Normalerweise werden Lesezeichen von Ihrem Browser in eine .html Datei exportiert. Wählen Sie die Datei hier aus und starten Sie den Import.<br>Abhängig von der Anzahl der Lesezeichen kann dieser Prozess einige Zeit in Anspruch nehmen.',
+
+    'import_networkerror' => 'Beim Importieren der Lesezeichen ist ein Fehler aufgetreten. Bitte überprüfen Sie die Konsole des Browsers oder die Logs der Anwendung für Details.',
+    'import_error' => 'Beim Importieren der Lesezeichen ist ein Fehler aufgetreten. Bitte prüfen Sie die Logs der Anwendung.',
+    'import_empty' => 'Konnte keine Lesezeichen importieren. Entweder ist die hochgeladene Datei beschädigt oder leer.',
+    'import_successfully' => ':imported Links erfolgreich importiert, :skipped übersprungen.',
+];
