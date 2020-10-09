d76664d9b1b187f0390a5cd6ca2dfe477ea67f6d
Kevin Woblick <mail@kovah.de>
2020-10-09 12:09:19 +0200

    New translations placeholder.php (Chinese Simplified)

---
+++ resources/lang/zh_CN/placeholder.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'username' => 'johndoe',
+    'email' => 'john.doe@example.com',
+    'password' => '密码',
+    'password_confirmed' => '已确认密码',
+
+    'link_url' => 'https://github.com/Kovah/LinkAce',
+    'link_title' => '网站标题',
+
+    'list_select' => '选择列表',
+    'tags_select' => '选择标签',
+
+    'list_name' => '列表的实际名称',
+    'tag_name' => '标签的实际名称',
+
+];
