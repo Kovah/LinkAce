818032e109297abde222ff78e2b8cab50fb0faf0
Kevin Woblick <mail@kovah.de>
2020-10-09 12:09:24 +0200

    New translations tag.php (Chinese Simplified)

---
+++ resources/lang/zh_CN/tag.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'tag' => '标签',
+    'tags' => '标签',
+    'all_tags' => '所有标签',
+
+    'add' => '添加标签',
+    'show' => '显示标签',
+    'edit' => '编辑标签',
+    'update' => '更新标签',
+    'delete' => '删除标签',
+
+    'private' => '私人标签',
+
+    'name' => '标签名',
+
+    'author' => '创建者：:user',
+
+    'no_tags' => '无标签',
+
+    'added_successfully' => '已添加标签。',
+    'updated_successfully' => '已更新标签。',
+    'deleted_successfully' => '已删除标签。',
+    'deletion_error' => '无法删除标签。',
+];
