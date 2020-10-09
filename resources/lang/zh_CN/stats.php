fc86b5bb939b5ea8789a20a3d376be0a932af2f2
Kevin Woblick <mail@kovah.de>
2020-10-09 12:09:25 +0200

    New translations stats.php (Chinese Simplified)

---
+++ resources/lang/zh_CN/stats.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'stats' => '统计',
+    'total_links' => '全部链接',
+    'total_lists' => '全部列表',
+    'total_tags' => '所有标签',
+    'total_notes' => '全部备注',
+    'total_broken_links' => '失效链接',
+
+];
