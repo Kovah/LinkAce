01c42f567a7aca647cbf660ea179ffd1ae0a1258
Kevin Woblick <mail@kovah.de>
2020-10-09 12:11:09 +0200

    New translations linkace.php (German)

---
+++ resources/lang/de_DE/linkace.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'linkace' => 'LinkAce',
+
+    'user' => 'Benutzer',
+    'username' => 'Benutzername',
+    'email' => 'E-Mail',
+    'password' => 'Passwort',
+    'password_confirm' => 'Passwort bestätigen',
+    'login' => 'Anmelden',
+    'logout' => 'Abmelden',
+    'remember_me' => 'Angemeldet bleiben',
+    'go_to_dashboard' => 'Zur Übersicht gehen',
+
+    'reset_password' => 'Passwort zurücksetzen',
+    'send_reset_email' => 'E-Mail zum Zurücksetzen des Passworts senden',
+    'forgot_password_link' => 'Passwort vergessen? <a href=":reset_url">Hier Zurücksetzen</a>',
+
+    'added' => 'Hinzugefügt',
+    'added_at' => 'Hinzugefügt am',
+    'updated_at' => 'Aktualisiert am',
+    'last_update' => 'Letzte Aktualisierung',
+    'deleted_at' => 'Gelöscht am',
+
+    'add' => 'Hinzufügen',
+    'show' => 'Anzeigen',
+    'edit' => 'Bearbeiten',
+    'update' => 'Aktualisieren',
+    'delete' => 'Löschen',
+
+    'menu' => 'Menü',
+    'entries' => 'Einträge',
+
+    'continue_adding' => 'Weiter hinzufügen',
+
+    'private' => 'Privat',
+    'is_private' => 'ist privat',
+
+    'yes' => 'Ja',
+    'no' => 'Nein',
+    'more' => 'Mehr',
+
+    'no_results_found' => 'Keine :model gefunden.',
+
+    'bookmarklet_close' => 'Das Bookmarklet-Fenster schließt automatisch in <span class="bm-timer">5</span> Sekunden.',
+    'open_linkace' => 'LinkAce öffnen',
+
+    'demo_login_hint' => 'Du kannst dich sofort einloggen. Bitte beachte, dass diese Demo alle 2 Stunden zurückgesetzt wird.',
+
+    'project_of' => 'Linkace ist ein Projekt von',
+    'version' => 'Aktuelle Version: :version',
+];
