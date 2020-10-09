064ea699d76729887c85a0263fc54e8017a30ed5
Kevin Woblick <mail@kovah.de>
2020-10-09 12:04:26 +0200

    New translations note.php (Catalan)

---
+++ resources/lang/ca_ES/note.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'note' => '',
+    'notes' => '',
+
+    'add' => '',
+    'show' => '',
+    'edit' => '',
+    'update' => '',
+    'delete' => '',
+
+    'private' => '',
+
+    'note_content' => '',
+
+    'added_successfully' => '',
+    'updated_successfully' => '',
+    'deleted_successfully' => '',
+    'deletion_error' => '',
+];
