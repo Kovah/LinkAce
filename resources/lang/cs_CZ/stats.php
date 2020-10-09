756e0193d2884f0d9f8035695f815fba1712e2d9
Kevin Woblick <mail@kovah.de>
2020-10-09 12:10:47 +0200

    New translations stats.php (Czech)

---
+++ resources/lang/cs_CZ/stats.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'stats' => '',
+    'total_links' => '',
+    'total_lists' => '',
+    'total_tags' => '',
+    'total_notes' => '',
+    'total_broken_links' => '',
+
+];
