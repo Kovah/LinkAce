c6b00b3643ac19bc027f21de963949b4e436ab72
Kevin Woblick <mail@kovah.de>
2020-10-09 12:06:43 +0200

    New translations trash.php (Serbian (Cyrillic))

---
+++ resources/lang/sr_SP/trash.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'trash' => '',
+
+    'deleted_links' => '',
+    'deleted_lists' => '',
+    'deleted_tags' => '',
+    'deleted_notes' => '',
+
+    'restore' => '',
+
+    'clear_trash' => '',
+    'delete_warning' => '',
+    'delete_no_entries' => '',
+
+    'delete_success.links' => '',
+    'delete_success.lists' => '',
+    'delete_success.tags' => '',
+    'delete_success.notes' => '',
+
+    'restore.link' => '',
+    'restore.list' => '',
+    'restore.tag' => '',
+    'restore.note' => '',
+
+    'restore.not_found' => '',
+    'restore.not_allowed' => '',
+
+];
