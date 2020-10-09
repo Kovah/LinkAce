cea229324729814b06c6b0d584647ffe92893779
Kevin Woblick <mail@kovah.de>
2020-10-09 12:10:37 +0200

    New translations link.php (Danish)

---
+++ resources/lang/da_DK/link.php
@@ -0,0 +1,64 @@
+<?php
+return [
+    'link' => '',
+    'links' => '',
+    'all_links' => '',
+    'recent_links' => '',
+    'no_links' => '',
+
+    'add' => '',
+    'add_quick' => '',
+    'show' => '',
+    'details' => '',
+    'edit' => '',
+    'update' => '',
+    'delete' => '',
+
+    'private' => '',
+
+    'history' => '',
+    'history_added' => '',
+    'history_changed' => '',
+    'history_removed' => '',
+    'history_deleted' => '',
+    'history_restored' => '',
+    'history_created' => '',
+
+    'url' => '',
+    'title' => '',
+    'description' => '',
+    'revtags' => '',
+    'revlists' => '',
+    'is_private' => '',
+
+    'status' => [
+        '1' => '',
+        '2' => '',
+        '3' => '',
+    ],
+
+    'author' => '',
+
+    'external_link' => '',
+    'wayback' => '',
+
+    'check_disable' => '',
+    'check_disabled' => '',
+    'check_enable' => '',
+    'check_enabled' => '',
+
+    'added_successfully' => '',
+    'added_connection_error' => '',
+    'added_request_error' => '',
+    'updated_successfully' => '',
+    'deleted_successfully' => '',
+    'deletion_error' => '',
+
+    'duplicates_found' => '',
+
+    'notifications.linkcheck.errors' => '',
+    'notifications.linkcheck.errors.moved' => '',
+    'notifications.linkcheck.errors.broken' => '',
+
+    'happy_bookmarking' => '',
+];
