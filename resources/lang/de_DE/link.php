ec5006d8b9386cadd83c0f466047b109d0698d0a
Kevin Woblick <mail@kovah.de>
2020-10-09 12:10:59 +0200

    New translations link.php (German)

---
+++ resources/lang/de_DE/link.php
@@ -0,0 +1,64 @@
+<?php
+return [
+    'link' => 'Link',
+    'links' => 'Links',
+    'all_links' => 'Alle Links',
+    'recent_links' => 'Letzte Links',
+    'no_links' => 'Keine Links',
+
+    'add' => 'Link hinzufügen',
+    'add_quick' => 'Link schnell hinzufügen',
+    'show' => 'Link anzeigen',
+    'details' => 'Link-Details',
+    'edit' => 'Link bearbeiten',
+    'update' => 'Link aktualisieren',
+    'delete' => 'Link löschen',
+
+    'private' => 'Privater Link',
+
+    'history' => 'Verlauf',
+    'history_added' => '<code>:newvalue</code> zu :fieldname hinzugefügt.',
+    'history_changed' => ':fieldname von <code>:oldvalue</code> zu <code>:newvalue geändert</code>',
+    'history_removed' => '<code>:oldvalue</code> von :fieldname entfernt.',
+    'history_deleted' => 'Link wurde gelöscht',
+    'history_restored' => 'Link wurde wiederhergestellt',
+    'history_created' => 'Link wurde erstellt',
+
+    'url' => 'URL',
+    'title' => 'Titel',
+    'description' => 'Beschreibung',
+    'revtags' => 'Tags',
+    'revlists' => 'Listen',
+    'is_private' => 'Privater Status',
+
+    'status' => [
+        '1' => 'Funktionierend',
+        '2' => 'Verschoben',
+        '3' => 'Kaputt',
+    ],
+
+    'author' => 'von :user',
+
+    'external_link' => 'Externer Link',
+    'wayback' => 'Archiv @ Wayback Machine',
+
+    'check_disable' => 'Prüfung deaktivieren',
+    'check_disabled' => 'Prüfung ist deaktiviert',
+    'check_enable' => 'Prüfung aktivieren',
+    'check_enabled' => 'Prüfung ist aktiviert',
+
+    'added_successfully' => 'Link erfolgreich hinzugefügt.',
+    'added_connection_error' => 'Der Link wurde hinzugefügt, aber beim Zugriff auf die URL ist ein Verbindungsfehler aufgetreten. Details finden Sie in den Logs.',
+    'added_request_error' => 'Der Link wurde hinzugefügt, aber ein Fehler trat auf, wenn versucht wurde, die URL anzufordern, zum Beispiel ein ungültiges Zertifikat. Details finden Sie in den Logs.',
+    'updated_successfully' => 'Link erfolgreich aktualisiert.',
+    'deleted_successfully' => 'Link erfolgreich gelöscht.',
+    'deletion_error' => 'Link konnte nicht gelöscht werden.',
+
+    'duplicates_found' => 'LinkAce hat mögliche Duplikate der URL gefunden:',
+
+    'notifications.linkcheck.errors' => 'LinkAce hat Fehler beim Überprüfen Ihrer Links gefunden.',
+    'notifications.linkcheck.errors.moved' => '⚠ Die folgenden Links wurden an einen neuen Ort verschoben:',
+    'notifications.linkcheck.errors.broken' => '🚫 Die folgenden Links sind nicht mehr erreichbar oder geben einen Fehler zurück:',
+
+    'happy_bookmarking' => 'Happy Bookmarking',
+];
