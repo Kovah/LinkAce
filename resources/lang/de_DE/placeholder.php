6d9560f8c6559f98cdb7519705298291d95b4398
Kevin Woblick <mail@kovah.de>
2020-10-09 12:11:03 +0200

    New translations placeholder.php (German)

---
+++ resources/lang/de_DE/placeholder.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'username' => 'maxmustermann',
+    'email' => 'max@mustermann.de',
+    'password' => 'Passwort',
+    'password_confirmed' => 'Passwort wiederholen',
+
+    'link_url' => 'https://github.com/Kovah/LinkAce',
+    'link_title' => 'Titel der Webseite',
+
+    'list_select' => 'Liste auswählen',
+    'tags_select' => 'Tags  auswählen',
+
+    'list_name' => 'Name der Liste',
+    'tag_name' => 'Name des Tags',
+
+];
