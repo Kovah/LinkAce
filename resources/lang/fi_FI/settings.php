758b1773d2a23735f272f73bd2ead6bb3276d4b6
Kevin Woblick <mail@kovah.de>
2020-10-09 12:10:13 +0200

    New translations settings.php (Finnish)

---
+++ resources/lang/fi_FI/settings.php
@@ -0,0 +1,88 @@
+<?php
+return [
+    'settings' => '',
+    'user_settings' => '',
+    'account_settings' => '',
+    'app_settings' => '',
+    'system_settings' => '',
+
+    'timezone' => '',
+    'date_format' => '',
+    'time_format' => '',
+    'listitem_count' => '',
+
+    'links_new_tab' => '',
+
+    'privacy' => '',
+    'links_private_default' => '',
+    'links_private_default_help' => '',
+    'notes_private_default' => '',
+    'notes_private_default_help' => '',
+    'tags_private_default' => '',
+    'tags_private_default_help' => '',
+    'lists_private_default' => '',
+    'lists_private_default_help' => '',
+
+    'archive_backups' => '',
+    'archive_backups_help' => '',
+    'archive_backups_enabled' => '',
+    'archive_backups_enabled_help' => '',
+    'archive_private_backups_enabled' => '',
+    'archive_private_backups_enabled_help' => '',
+
+    'display_mode' => '',
+    'display_mode_list_detailed' => '',
+    'display_mode_list_simple' => '',
+    'display_mode_cards' => '',
+
+    'sharing' => '',
+    'sharing_help' => '',
+    'sharing_toggle' => '',
+
+    'darkmode' => '',
+    'darkmode_help' => '',
+    'darkmode_disabled' => '',
+    'darkmode_auto' => '',
+    'darkmode_permanent' => '',
+
+    'save_settings' => '',
+    'settings_saved' => '',
+
+    'bookmarklet' => '',
+    'bookmarklet_button' => '',
+    'bookmarklet_help' => '',
+
+    'change_password' => '',
+    'old_password' => '',
+    'new_password' => '',
+    'new_password2' => '',
+    'password_updated' => '',
+    'old_password_invalid' => '',
+
+    'api_token' => '',
+    'api_token_generate' => '',
+    'api_token_generate_confirm' => '',
+    'api_token_help' => '',
+    'api_token_generate_info' => '',
+    'api_token_generate_failure' => '',
+
+    'sys_page_title' => '',
+    'sys_guest_access' => '',
+    'sys_guest_access_help' => '',
+
+    'cron_token' => '',
+    'cron_token_generate' => '',
+    'cron_token_generate_confirm' => '',
+    'cron_token_help' => '',
+    'cron_token_url' => '',
+    'cron_token_generate_info' => '',
+    'cron_token_generate_failure' => '',
+    'cron_token_auth_failure' => '',
+    'cron_execute_successful' => '',
+
+    'update_check' => '',
+    'update_check_running' => '',
+    'update_check_version_found' => '',
+    'update_check_success' => '',
+    'update_check_failed' => '',
+];
