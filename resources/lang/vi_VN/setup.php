16a37c358fc90703c63bebb1c35e43f22530b2a3
Kevin Woblick <mail@kovah.de>
2020-10-09 12:07:44 +0200

    New translations setup.php (Vietnamese)

---
+++ resources/lang/vi_VN/setup.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    'setup' => '',
+    'continue' => '',
+    'try_again' => '',
+
+    'welcome' => '',
+    'intro' => '',
+    'intro.step1' => '',
+    'intro.step2' => '',
+    'intro.step3' => '',
+
+    'check_requirements' => '',
+    'requirements.php_version' => '',
+    'requirements.extension_bcmath' => '',
+    'requirements.extension_ctype' => '',
+    'requirements.extension_json' => '',
+    'requirements.extension_mbstring' => '',
+    'requirements.extension_openssl' => '',
+    'requirements.extension_pdo_mysql' => '',
+    'requirements.extension_tokenizer' => '',
+    'requirements.extension_xml' => '',
+    'requirements.env_writable' => '',
+
+    'database_configuration' => '',
+    'database_configure' => '',
+    'database.intro' => '',
+    'database.config_error' => '',
+    'database.db_host' => '',
+    'database.db_port' => '',
+    'database.db_name' => '',
+    'database.db_user' => '',
+    'database.db_password' => '',
+    'database.complete_hint' => '',
+
+    'database.data_present' => '',
+    'database.overwrite_data' => '',
+
+    'account_setup' => '',
+    'account_setup.intro' => '',
+    'account_setup.name' => '',
+    'account_setup.email' => '',
+    'account_setup.password' => '',
+    'account_setup.password_requirements' => '',
+    'account_setup.password_confirmed' => '',
+    'account_setup.create' => '',
+
+    'complete' => '',
+    'outro' => '',
+];
