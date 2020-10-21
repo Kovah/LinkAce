ddfee1d7865260cfc1a7ef1d9bbca71e6ef4e26d
Kevin Woblick <mail@kovah.de>
2020-10-22 00:31:03 +0200

    New translations search.php (Romanian)

---
+++ resources/lang/ro_RO/search.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'search' => '',
+    'results' => '',
+
+    'filter_by_list' => '',
+    'filter_by_tag' => '',
+    'query' => '',
+    'search_title' => '',
+    'search_description' => '',
+    'private_only' => '',
+    'broken_links' => '',
+
+    'order_by' => '',
+    'order_by.title:asc' => '',
+    'order_by.title:desc' => '',
+    'order_by.url:asc' => '',
+    'order_by.url:desc' => '',
+    'order_by.created_at:asc' => '',
+    'order_by.created_at:desc' => '',
+    'order_by.number_links:asc' => '',
+    'order_by.number_links:desc' => '',
+
+    'no_results' => '',
+
+    'validation_query_missing' => '',
+    'validation_lists_missing' => '',
+    'validation_tags_missing' => '',
+];
