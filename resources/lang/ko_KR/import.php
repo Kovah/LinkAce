312d2e1b5cab93f5d7899b0c0a7cbfabf62c6ade
Kevin Woblick <mail@kovah.de>
2020-10-22 00:36:14 +0200

    New translations import.php (Korean)

---
+++ resources/lang/ko_KR/import.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'import' => '',
+    'start_import' => '',
+    'import_running' => '',
+    'import_file' => '',
+
+    'import_help' => '',
+
+    'import_networkerror' => '',
+    'import_error' => '',
+    'import_empty' => '',
+    'import_successfully' => '',
+];
