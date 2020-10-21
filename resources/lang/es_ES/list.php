80d0007ed44a721e5f4878d1de4426233f7a0778
Kevin Woblick <mail@kovah.de>
2020-10-22 00:41:01 +0200

    New translations list.php (Spanish)

---
+++ resources/lang/es_ES/list.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'list' => '',
+    'lists' => '',
+    'all_lists' => '',
+
+    'add' => '',
+    'show' => '',
+    'edit' => '',
+    'update' => '',
+    'delete' => '',
+
+    'private' => '',
+
+    'name' => '',
+    'description' => '',
+
+    'author' => '',
+
+    'no_lists' => '',
+
+    'number_links' => '',
+
+    'added_successfully' => '',
+    'updated_successfully' => '',
+    'deleted_successfully' => '',
+    'deletion_error' => '',
+];
