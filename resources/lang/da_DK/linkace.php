d800dedffe4d9cdfb0218a158807e4ed04c29d7f
Kevin Woblick <mail@kovah.de>
2020-10-22 00:36:42 +0200

    New translations linkace.php (Danish)

---
+++ resources/lang/da_DK/linkace.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'linkace' => '',
+
+    'user' => '',
+    'username' => '',
+    'email' => '',
+    'password' => '',
+    'password_confirm' => '',
+    'login' => '',
+    'logout' => '',
+    'remember_me' => '',
+    'go_to_dashboard' => '',
+
+    'reset_password' => '',
+    'send_reset_email' => '',
+    'forgot_password_link' => '',
+
+    'added' => '',
+    'added_at' => '',
+    'updated_at' => '',
+    'last_update' => '',
+    'deleted_at' => '',
+
+    'add' => '',
+    'show' => '',
+    'edit' => '',
+    'update' => '',
+    'delete' => '',
+
+    'menu' => '',
+    'entries' => '',
+
+    'continue_adding' => '',
+
+    'private' => '',
+    'is_private' => '',
+
+    'yes' => '',
+    'no' => '',
+    'more' => '',
+
+    'no_results_found' => '',
+
+    'bookmarklet_close' => '',
+    'open_linkace' => '',
+
+    'demo_login_hint' => '',
+
+    'project_of' => '',
+    'version' => '',
+];
