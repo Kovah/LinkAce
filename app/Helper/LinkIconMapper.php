479613aa5dd2f58794df5011b84f80dbc51259d7
Kovah <mail@kovah.de>
2020-10-21 13:46:18 +0200

    Change mapping of icons for links and social sharing (#158)

--- app/Helper/LinkIconMapper.php
+++ app/Helper/LinkIconMapper.php
@@ -10,15 +10,12 @@ namespace App\Helper;
 class LinkIconMapper
 {
     /** @var string Default icon as fallback if no specific icon was found */
-    public static $default_icon = 'fa fa-link';
+    public static $defaultIcon = 'link';
 
     /** @var array Array holding the pattern => icon map, sorted alphabetically */
-    public static $icon_map = [
+    public static $iconMap = [
         '500px\.com' => 'fab fa-500px',
-        'adobe\.com' => 'fab fa-adobe',
-        'aws\.amazon\.com' => 'fab fa-aws', // put before amazon pattern to keep it easy
-        'amazon\.([a-zA-Z]*(\.)?[a-zA-Z]*)' => 'fab fa-amazon', // match amazon with every TLD
-        'angel\.co' => 'fab fa-angellist',
+        'amazon\.([a-zA-Z]*(\.)?[a-zA-Z]*)' => 'fab fa-amazon',
         'itunes\.apple\.com\/app' => 'fab fa-app-store',
         'apple\.com' => 'fab fa-apple',
         'artstation\.com' => 'fab fa-artstation',
@@ -32,22 +29,16 @@ class LinkIconMapper
         'codepen\.io' => 'fab fa-codepen',
         'dev\.to' => 'fab fa-dev',
         'deviantart\.com' => 'fab fa-deviantart',
-        'digitalocean\.com' => 'fab fa-digital-ocean',
         '(discordapp\.com|discord\.gg)' => 'fab fa-discord',
-        'meta\.discourse\.org' => 'fab fa-discourse',
         'docker\.com' => 'fab fa-docker',
         'dribbble\.com' => 'fab fa-dribbble',
         'dropbox\.com' => 'fab fa-dropbox',
-        'drupal\.org' => 'fab fa-drupal',
         'ebay\.([a-zA-Z]*(\.)?[a-zA-Z]*)' => 'fab fa-ebay',
-        'ello\.co' => 'fab fa-ello',
         'etsy\.com' => 'fab fa-etsy',
-        '(facebook\.com|fb\.me)' => 'fab fa-facebook-f',
+        '(facebook\.com|fb\.me)' => 'fab fa-facebook-square',
         'addons\.mozilla\.org\/[a-zA-Z-_]+\/firefox' => 'fab fa-firefox',
-        'figma\.com' => 'fab fa-figma',
         'flickr\.com' => 'fab fa-flickr',
         '(flipboard\.com|flip\.it)' => 'fab fa-flipboard',
-        'foursquare\.com' => 'fab fa-foursquare',
         '(getpocket\.com|pocket\.co)' => 'fab fa-get-pocket',
         '(github\.com|github\.io)' => 'fab fa-github',
         'gitlab\.com' => 'fab fa-gitlab',
@@ -55,18 +46,13 @@ class LinkIconMapper
         'drive\.google\.com' => 'fab fa-google-drive',
         'play\.google\.com' => 'fab fa-google-play',
         'google\.([a-zA-Z]*(\.)?[a-zA-Z]*)' => 'fab fa-google',
-        'news\.ycombinator\.com' => 'fab fa-hacker-news',
-        'hackerrank\.com' => 'fab fa-hackerrank',
-        'houzz\.com' => 'fab fa-houzz',
+        'news\.ycombinator\.com' => 'fab fa-hacker-news-square',
         'imdb\.com' => 'fab fa-imdb',
         'instagram\.com' => 'fab fa-instagram',
-        'invisionapp\.com' => 'fab fa-invision',
         'jsfiddle\.net' => 'fab fa-jsfiddle',
-        'kaggle\.com' => 'fab fa-kaggle',
         'keybase\.io' => 'fab fa-keybase',
         'kickstarter\.com' => 'fab fa-kickstarter',
         'last\.fm' => 'fab fa-lastfm',
-        'leanpub\.com' => 'fab fa-leanpub',
         'linkedin\.com' => 'fab fa-linkedin-in',
         'mastodon\.social' => 'fab fa-mastodon',
         'medium\.com' => 'fab fa-medium-m',
@@ -75,46 +61,36 @@ class LinkIconMapper
         'mixcloud\.com' => 'fab fa-mixcloud',
         'npmjs\.com' => 'fab fa-npm',
         'openid\.net' => 'fab fa-openid',
-        'opensource\.org' => 'fab fa-osi',
         'patreon\.com' => 'fab fa-patreon ',
         'paypal\.([a-zA-Z]*(\.)?[a-zA-Z]*)' => 'fab fa-paypal',
-        'periscope\.tv' => 'fab fa-periscope',
         'php\.net' => 'fab fa-php',
-        'pinterest\.com' => 'fab fa-pinterest',
+        'pinterest\.com' => 'fab fa-pinterest-square',
         'playstation\.com' => 'fab fa-playstation',
         'producthunt\.com' => 'fab fa-product-hunt',
-        'python\.org' => 'fab fa-python',
         'quora\.com' => 'fab fa-quora',
-        'readme\.io' => 'fab fa-readme',
-        'reddit\.com' => 'fab fa-reddit',
+        'reddit\.com' => 'fab fa-reddit-square',
         'researchgate\.net' => 'fab fa-researchgate',
-        'rocket\.chat' => 'fab fa-rocketchat',
-        'scribd\.com' => 'fab fa-scribd',
         'skype\.com' => 'fab fa-skype',
         'slack\.com' => 'fab fa-slack',
         'slideshare\.net' => 'fab fa-slideshare',
         'snapchat\.com' => 'fab fa-snapchat',
         'soundcloud\.com' => 'fab fa-soundcloud',
         'spotify\.com' => 'fab fa-spotify',
-        'squarespace\.com' => 'fab fa-squarespace',
         'stackexchange\.com' => 'fab fa-stack-exchange',
         'stackoverflow\.com' => 'fab fa-stack-overflow',
         '(steampowered\.com|steamcommunity\.com)' => 'fab fa-steam',
-        'strava\.com' => 'fab fa-strava',
         '(telegram\.org|t\.me)' => 'fab fa-telegram',
         'trello\.com' => 'fab fa-trello',
         'tripadvisor\.com' => 'fab fa-tripadvisor',
-        'tumblr\.com' => 'fab fa-tumblr',
+        'tumblr\.com' => 'fab fa-tumblr-square',
         'twitch\.tv' => 'fab fa-twitch',
-        'twitter\.com' => 'fab fa-twitter',
+        'twitter\.com' => 'fab fa-twitter-square',
         'vimeo\.com' => 'fab fa-vimeo',
         'weibo\.com' => 'fab fa-weibo',
         'wikipedia\.org' => 'fab fa-wikipedia-w',
-        'wordpress\.com' => 'fab fa-wordpress',
-        'wordpress\.org' => 'fab fa-wordpress-simple',
+        'wordpress\.(com|org)' => 'fab fa-wordpress',
         'xbox\.com' => 'fab fa-xbox',
-        'xing\.com' => 'fab fa-xing',
-        'yahoo\.com' => 'fab fa-yahoo',
+        'xing\.com' => 'fab fa-xing-square',
         'yelp\.com' => 'fab fa-yelp',
         'youtube\.com' => 'fab fa-youtube',
     ];
@@ -127,12 +103,12 @@ class LinkIconMapper
      */
     public static function mapLink(string $url): string
     {
-        foreach (self::$icon_map as $pattern => $icon) {
+        foreach (self::$iconMap as $pattern => $icon) {
             if (preg_match('/' . $pattern . '/i', $url)) {
                 return $icon;
             }
         }
 
-        return self::$default_icon;
+        return self::$defaultIcon;
     }
 }
---
+++ database/migrations/2020_10_21_104548_migrate_link_icons.php
@@ -0,0 +1,118 @@
+<?php
+
+use App\Helper\LinkIconMapper;
+use App\Models\Link;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrateLinkIcons extends Migration
+{
+    /** @var array */
+    protected $changedLinks = [
+        'fab fa-500px' => 'brand.500px',
+        'fab fa-amazon' => 'brand.amazon',
+        'fab fa-app-store' => 'brand.app',
+        'fab fa-apple' => 'brand.apple',
+        'fab fa-artstation' => 'brand.artstation',
+        'fab fa-atlassian' => 'brand.atlassian',
+        'fab fa-bandcamp' => 'brand.bandcamp',
+        'fab fa-behance' => 'brand.behance',
+        'fab fa-bitbucket' => 'brand.bitbucket',
+        'fab fa-bitcoin' => 'brand.bitcoin',
+        'fab fa-blogger-b' => 'brand.blogger-b',
+        'fab fa-chrome' => 'brand.chrome',
+        'fab fa-codepen' => 'brand.codepen',
+        'fab fa-dev' => 'brand.dev',
+        'fab fa-deviantart' => 'brand.deviantart',
+        'fab fa-discord' => 'brand.discord',
+        'fab fa-docker' => 'brand.docker',
+        'fab fa-dribbble' => 'brand.dribbble',
+        'fab fa-dropbox' => 'brand.dropbox',
+        'fab fa-ebay' => 'brand.ebay',
+        'fab fa-etsy' => 'brand.etsy',
+        'fab fa-facebook-square' => 'brand.facebook',
+        'fab fa-firefox' => 'brand.firefox',
+        'fab fa-flickr' => 'brand.flickr',
+        'fab fa-flipboard' => 'brand.flipboard',
+        'fab fa-get-pocket' => 'brand.get-pocket',
+        'fab fa-github' => 'brand.github',
+        'fab fa-gitlab' => 'brand.gitlab',
+        'fab fa-gitter' => 'brand.gitter',
+        'fab fa-google' => 'brand.google',
+        'fab fa-google-drive' => 'brand.google-drive',
+        'fab fa-google-play' => 'brand.google-play',
+        'fab fa-hacker-news' => 'brand.hacker-news',
+        'fab fa-hacker-news-square' => 'brand.hacker-news',
+        'fab fa-imdb' => 'brand.imdb',
+        'fab fa-instagram' => 'brand.instagram',
+        'fab fa-jsfiddle' => 'brand.jsfiddle',
+        'fab fa-keybase' => 'brand.keybase',
+        'fab fa-kickstarter' => 'brand.kickstarter',
+        'fab fa-lastfm' => 'brand.lastfm',
+        'fab fa-linkedin-in' => 'brand.linkedin-in',
+        'fab fa-mastodon' => 'brand.mastodon',
+        'fab fa-medium-m' => 'brand.medium-m',
+        'fab fa-meetup' => 'brand.meetup',
+        'fab fa-microsoft' => 'brand.microsoft',
+        'fab fa-mixcloud' => 'brand.mixcloud',
+        'fab fa-npm' => 'brand.npm',
+        'fab fa-openid' => 'brand.openid',
+        'fab fa-patreon ' => 'brand.patreon',
+        'fab fa-paypal' => 'brand.paypal',
+        'fab fa-php' => 'brand.php',
+        'fab fa-pinterest-square' => 'brand.pinterest',
+        'fab fa-playstation' => 'brand.playstation',
+        'fab fa-product-hunt' => 'brand.product',
+        'fab fa-quora' => 'brand.quora',
+        'fab fa-reddit-square' => 'brand.reddit',
+        'fab fa-researchgate' => 'brand.researchgate',
+        'fab fa-skype' => 'brand.skype',
+        'fab fa-slack' => 'brand.slack',
+        'fab fa-slideshare' => 'brand.slideshare',
+        'fab fa-snapchat' => 'brand.snapchat',
+        'fab fa-soundcloud' => 'brand.soundcloud',
+        'fab fa-spotify' => 'brand.spotify',
+        'fab fa-stack-exchange' => 'brand.stack-exchange',
+        'fab fa-stack-overflow' => 'brand.stack-overflow',
+        'fab fa-steam' => 'brand.steam',
+        'fab fa-telegram' => 'brand.telegram',
+        'fab fa-trello' => 'brand.trello',
+        'fab fa-tripadvisor' => 'brand.tripadvisor',
+        'fab fa-tumblr-square' => 'brand.tumblr',
+        'fab fa-twitch' => 'brand.twitch',
+        'fab fa-twitter-square' => 'brand.twitter',
+        'fab fa-vimeo' => 'brand.vimeo',
+        'fab fa-weibo' => 'brand.weibo',
+        'fab fa-wikipedia-w' => 'brand.wikipedia-w',
+        'fab fa-wordpress' => 'brand.wordpress',
+        'fab fa-xbox' => 'brand.xbox',
+        'fab fa-xing-square' => 'brand.xing',
+        'fab fa-yelp' => 'brand.yelp',
+        'fab fa-youtube' => 'brand.youtube',
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Link::withTrashed()->chunk(100, function ($links) {
+            /** @var Link $link */
+            foreach ($links as $link) {
+                $link->icon = $this->changedLinks[$link->icon] ?? LinkIconMapper::$defaultIcon;
+                $link->save();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
