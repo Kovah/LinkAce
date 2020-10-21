f0a280f97c8b79391a191b04265f6259ea9757e0
Kevin Woblick <mail@kovah.de>
2020-10-22 00:31:14 +0200

    New translations placeholder.php (Serbian (Cyrillic))

---
+++ resources/lang/sr_SP/placeholder.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'username' => '',
+    'email' => '',
+    'password' => '',
+    'password_confirmed' => '',
+
+    'link_url' => '',
+    'link_title' => '',
+
+    'list_select' => '',
+    'tags_select' => '',
+
+    'list_name' => '',
+    'tag_name' => '',
+
+];
