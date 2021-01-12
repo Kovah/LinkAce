ff880de8d6fde06493d93b1eb2dc1ccc8735870e
Kovah <mail@kovah.de>
2021-01-12 23:37:54 +0100

    Correct logo sizing on the login splash-page (#212)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,10 @@
             <div class="col-12 col-md-8 text-center">
 
                 <img src="{{ asset('assets/img/linkace_logo.svg') }}" alt="@lang('linkace.linkace')"
-                    width="400" height="147" class="mt-5 mb-5">
-
-                <br>
+                    width="400" height="147" class="my-5 px-3 px-sm-5 mw-100 h-auto">
 
                 <a href="{{ route('login') }}" class="btn btn-lg btn-primary">
-                    <x-icon.ban class="mr-2"/> @lang('linkace.login')
+                    <x-icon.unlock class="mr-2"/> @lang('linkace.login')
                 </a>
 
             </div>
