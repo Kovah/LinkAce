99d5beed1c792a1af79e6cfb9a154f18572d6867
Kovah <mail@kovah.de>
2021-03-05 22:53:46 +0100

    Update config for Enlightn

--- config/enlightn.php
+++ config/enlightn.php
@@ -115,4 +115,21 @@ return [
         storage_path(),
         app()->bootstrapPath('cache'),
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Credentials
+    |--------------------------------------------------------------------------
+    |
+    | The following credentials are used to share your Enlightn report with
+    | the Enlightn Github Bot. This allows the bot to compile the report
+    | and add review comments on your pull requests.
+    |
+    */
+    'credentials' => [
+        'username' => env('ENLIGHTN_USERNAME'),
+        'api_token' => env('ENLIGHTN_API_TOKEN'),
+    ],
+    'github_repo' => env('ENLIGHTN_GITHUB_REPO', 'Kovah/LinkAce'),
+
 ];
