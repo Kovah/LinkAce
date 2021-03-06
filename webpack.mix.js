feb887d582949c62d77f607aed90e32dbd6fbadd
Kovah <mail@kovah.de>
2021-03-06 19:57:08 +0100

    Update dependencies, upgrade to Laravel Mix 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,23 +2,19 @@ const mix = require('laravel-mix');
 
 mix.options({
   processCssUrls: false
-});
-
-mix.disableNotifications();
-
-mix.setPublicPath('public');
+})
+  .disableNotifications()
+  .setPublicPath('public')
+  .version();
 
 mix.js('resources/assets/js/app.js', 'assets/dist/js')
-  .sourceMaps()
-  .version();
+  .sourceMaps();
 
 mix.combine([
   'node_modules/jquery/dist/jquery.min.js',
   'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
   'node_modules/selectize/dist/js/standalone/selectize.js'
-], 'public/assets/dist/js/dependencies.js')
-  .version();
+], 'public/assets/dist/js/dependencies.js');
 
 mix.sass('resources/assets/sass/app.scss', 'assets/dist/css')
-  .sass('resources/assets/sass/app-dark.scss', 'assets/dist/css')
-  .version();
+  .sass('resources/assets/sass/app-dark.scss', 'assets/dist/css');
