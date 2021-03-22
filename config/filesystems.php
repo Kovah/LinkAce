a15d464c94fc6ed2d6d742a3436ca9fdf29a90a7
Kovah <mail@kovah.de>
2021-03-22 21:41:50 +0100

    Update configuration to support third-party s3 services (#198)

--- config/filesystems.php
+++ config/filesystems.php
@@ -72,6 +72,7 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
         ],
 
     ],
