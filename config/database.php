343a4423e204e50b1a7421b0b290862e8c8d13a8
Kovah <mail@kovah.de>
2021-03-22 22:03:38 +0100

    Update configuration to support Redis sockets (#195)

--- config/database.php
+++ config/database.php
@@ -144,6 +144,16 @@ return [
             'database' => env('REDIS_CACHE_DB', 1),
         ],
 
+        'redis-socket' => [
+            'scheme' => env('REDIS_SCHEME'),
+            'path' => env('REDIS_PATH'),
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
+
     ],
 
 ];
