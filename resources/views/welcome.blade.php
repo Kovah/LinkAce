3752db784823fbc6d8672a4a74ffcec0675d5636
Kovah <mail@kovah.de>
2021-04-19 18:39:44 +0200

    Hotfixes for the component linkace logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,9 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-8 text-center">
 
-                <img src="{{ asset('assets/img/linkace_logo.svg') }}" alt="@lang('linkace.linkace')"
-                    width="400" height="147" class="my-5 px-3 px-sm-5 mw-100 h-auto">
+                <div class="mb-5 display-2 text-center text-primary">
+                    <x-icon.linkace/>
+                </div>
 
                 <a href="{{ route('login') }}" class="btn btn-lg btn-primary">
                     <x-icon.unlock class="mr-2"/> @lang('linkace.login')
