69bef0101be0255ed98ff92ad4a8a7e924294d22
Kovah <mail@kovah.de>
2022-01-06 10:12:08 +0100

    Handle duplicate error messages (#329)

--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -24,10 +24,10 @@ class SearchRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'query' => 'bail|required_without_all:only_lists,only_tags,broken_only',
-            'only_lists' => 'bail|required_without_all:query,only_tags,broken_only',
-            'only_tags' => 'bail|required_without_all:query,only_lists,broken_only',
-            'broken_only' => 'bail|required_without_all:query,only_lists,only_tags',
+            'query' => 'required_without_all:only_lists,only_tags,broken_only',
+            'only_lists' => 'required_without_all:query,only_tags,broken_only',
+            'only_tags' => 'required_without_all:query,only_lists,broken_only',
+            'broken_only' => 'required_without_all:query,only_lists,only_tags',
         ];
     }
 
