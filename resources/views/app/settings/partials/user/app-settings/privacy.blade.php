22b74696d7893eae18efe20f6c27aa46f03bc2de
Kovah <mail@kovah.de>
2022-06-24 22:56:43 +0200

    Update the settings for model visibility (#165)

--- resources/views/app/settings/partials/user/app-settings/privacy.blade.php
+++ resources/views/app/settings/partials/user/app-settings/privacy.blade.php
@@ -1,116 +1,53 @@
+<?php
+
+use App\Enums\ModelAttribute;
+
+$settings = [
+    'links_default_visibility',
+    'notes_default_visibility',
+    'lists_default_visibility',
+    'tags_default_visibility',
+];
+?>
 <div class="mb-3 my-5">
     <h5 class="mb-3">
         @lang('settings.privacy')
     </h5>
 
-    <div class="row">
-        <div class="col-12 col-sm-8 col-md-6">
-
-            <div class="mb-4">
-                <label class="form-label" for="links_private_default">
-                    @lang('settings.links_private_default')
-                </label>
-                <select id="links_private_default" name="links_private_default"
-                    class="form-select{{ $errors->has('links_private_default') ? ' is-invalid' : '' }}">
-                    <option value="0"
-                        @if(usersettings('links_private_default') === false) selected @endif>
-                        @lang('linkace.no')
-                    </option>
-                    <option value="1"
-                        @if(usersettings('links_private_default') === true) selected @endif>
-                        @lang('linkace.yes')
-                    </option>
-                </select>
-                <p class="text-muted small mt-1">@lang('settings.links_private_default_help')</p>
-                @if ($errors->has('links_private_default'))
-                    <p class="invalid-feedback" role="alert">
-                        {{ $errors->first('links_private_default') }}
-                    </p>
-                @endif
-            </div>
-
-        </div>
-        <div class="col-12 col-sm-8 col-md-6">
-
-            <div class="mb-4">
-                <label class="form-label" for="notes_private_default">
-                    @lang('settings.notes_private_default')
-                </label>
-                <select id="notes_private_default" name="notes_private_default"
-                    class="form-select{{ $errors->has('notes_private_default') ? ' is-invalid' : '' }}">
-                    <option value="0"
-                        @if(usersettings('notes_private_default') === false) selected @endif>
-                        @lang('linkace.no')
-                    </option>
-                    <option value="1"
-                        @if(usersettings('notes_private_default') === true) selected @endif>
-                        @lang('linkace.yes')
-                    </option>
-                </select>
-                <p class="text-muted small mt-1">@lang('settings.notes_private_default_help')</p>
-                @if ($errors->has('notes_private_default'))
-                    <p class="invalid-feedback" role="alert">
-                        {{ $errors->first('notes_private_default') }}
-                    </p>
-                @endif
-            </div>
-
-        </div>
-    </div>
+    <p>@lang('settings.default_visibility_help')</p>
 
     <div class="row">
-        <div class="col-12 col-sm-8 col-md-6">
+        @foreach($settings as $setting)
+            <div class="col-12 col-sm-8 col-md-6">
 
-            <div class="mb-4">
-                <label class="form-label" for="lists_private_default">
-                    @lang('settings.lists_private_default')
-                </label>
-                <select id="lists_private_default" name="lists_private_default"
-                    class="form-select{{ $errors->has('lists_private_default') ? ' is-invalid' : '' }}">
-                    <option value="0"
-                        @if(usersettings('lists_private_default') === false) selected @endif>
-                        @lang('linkace.no')
-                    </option>
-                    <option value="1"
-                        @if(usersettings('lists_private_default') === true) selected @endif>
-                        @lang('linkace.yes')
-                    </option>
-                </select>
-                <p class="text-muted small mt-1">@lang('settings.lists_private_default_help')</p>
-                @if ($errors->has('lists_private_default'))
-                    <p class="invalid-feedback" role="alert">
-                        {{ $errors->first('lists_private_default') }}
-                    </p>
-                @endif
+                <div class="mb-4">
+                    <label class="form-label" for="{{ $setting }}">
+                        @lang('settings.' . $setting)
+                    </label>
+                    <select id="{{ $setting }}" name="{{ $setting }}"
+                        class="form-select{{ $errors->has($setting) ? ' is-invalid' : '' }}">
+                        <option value="{{ ModelAttribute::VISIBILITY_PUBLIC }}"
+                            {{ usersettings($setting) === ModelAttribute::VISIBILITY_PUBLIC ? 'selected' : '' }}>
+                            @lang('attributes.visibility.' . ModelAttribute::VISIBILITY_PUBLIC)
+                        </option>
+                        <option value="{{ ModelAttribute::VISIBILITY_INTERNAL }}"
+                            {{ usersettings($setting) === ModelAttribute::VISIBILITY_INTERNAL ? 'selected' : '' }}>
+                            @lang('attributes.visibility.' . ModelAttribute::VISIBILITY_INTERNAL)
+                        </option>
+                        <option value="{{ ModelAttribute::VISIBILITY_PRIVATE }}"
+                            {{ usersettings($setting) === ModelAttribute::VISIBILITY_PRIVATE ? 'selected' : '' }}>
+                            @lang('attributes.visibility.' . ModelAttribute::VISIBILITY_PRIVATE)
+                        </option>
+                    </select>
+                    @if ($errors->has($setting))
+                        <p class="invalid-feedback" role="alert">
+                            {{ $errors->first($setting) }}
+                        </p>
+                    @endif
+                </div>
+
             </div>
-
-        </div>
-        <div class="col-12 col-sm-8 col-md-6">
-
-            <div class="mb-4">
-                <label class="form-label" for="tags_private_default">
-                    @lang('settings.tags_private_default')
-                </label>
-                <select id="tags_private_default" name="tags_private_default"
-                    class="form-select{{ $errors->has('tags_private_default') ? ' is-invalid' : '' }}">
-                    <option value="0"
-                        @if(usersettings('tags_private_default') === false) selected @endif>
-                        @lang('linkace.no')
-                    </option>
-                    <option value="1"
-                        @if(usersettings('tags_private_default') === true) selected @endif>
-                        @lang('linkace.yes')
-                    </option>
-                </select>
-                <p class="text-muted small mt-1">@lang('settings.tags_private_default_help')</p>
-                @if ($errors->has('tags_private_default'))
-                    <p class="invalid-feedback" role="alert">
-                        {{ $errors->first('tags_private_default') }}
-                    </p>
-                @endif
-            </div>
-
-        </div>
+        @endforeach
     </div>
 
 </div>
