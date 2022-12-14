a3a7209fe14f79e44fbe77f25b75d338477fe1d4
Kovah <mail@kovah.de>
2022-12-14 23:48:20 +0100

    Correctly toggle share settings (#581)

--- resources/assets/js/components/ShareToggleAll.js
+++ resources/assets/js/components/ShareToggleAll.js
@@ -2,14 +2,16 @@ export default class ShareToggleAll {
 
   constructor ($el) {
     this.$toggle = $el;
+    this.state = false;
     this.shareToggles = document.documentElement.querySelectorAll('.sharing-checkbox-input');
 
     this.$toggle.addEventListener('click', this.onToggleClick.bind(this));
   }
 
   onToggleClick () {
+    this.state = !this.state
     this.shareToggles.forEach(toggle => {
-      toggle.checked = !toggle.checked;
+      toggle.checked = this.state;
     })
   }
 }
