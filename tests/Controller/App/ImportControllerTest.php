ada6d5ed3f812cf890de50db8d59888774376995
Kovah <mail@kovah.de>
2024-02-20 10:45:19 +0100

    Add default date for imported bookmarks if there's no date in the export (#752)

--- tests/Controller/App/ImportControllerTest.php
+++ tests/Controller/App/ImportControllerTest.php
@@ -6,6 +6,7 @@ use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
@@ -44,6 +45,25 @@ class ImportControllerTest extends TestCase
         $this->assertDatabaseCount('tags', 18);
     }
 
+    public function testDatelessImportActionResponse(): void
+    {
+        $this->travelTo(Carbon::create(2024, 2, 20));
+
+        $response = $this->importBookmarks('/data/import_example_dateless.html');
+
+        $response->assertOk()
+            ->assertJson([
+                'success' => true,
+            ]);
+
+        $this->assertDatabaseCount('links', 5);
+        $this->assertDatabaseCount('tags', 18);
+        $this->assertDatabaseHas('links', [
+            'url' => 'https://loader.io/',
+            'created_at' => '2024-02-20 00:00:00'
+        ]);
+    }
+
     public function testImportWithPrivateDefaults(): void
     {
         Setting::create(['user_id' => 1, 'key' => 'links_private_default', 'value' => '1']);
@@ -69,12 +89,12 @@ class ImportControllerTest extends TestCase
         ]);
     }
 
-    protected function importBookmarks(): TestResponse
+    protected function importBookmarks(string $importFile = '/data/import_example.html'): TestResponse
     {
         $testHtml = '<!DOCTYPE html><head><title>DuckDuckGo</title></head></html>';
         Http::fake(['*' => Http::response($testHtml)]);
 
-        $exampleData = file_get_contents(__DIR__ . '/data/import_example.html');
+        $exampleData = file_get_contents(__DIR__ . $importFile);
         $file = UploadedFile::fake()->createWithContent('import_example.html', $exampleData);
 
         return $this->post('import', [
