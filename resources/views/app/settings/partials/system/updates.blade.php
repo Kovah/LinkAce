0e7477ce73b11394ac6522381257a7dc8fe9fc22
Kovah <mail@kovah.de>
2024-09-23 23:11:52 +0200

    Add Linkace 2 Beta hint to system settings

--- resources/views/app/settings/partials/system/updates.blade.php
+++ resources/views/app/settings/partials/system/updates.blade.php
@@ -6,6 +6,9 @@
         <p>@lang('linkace.version', ['version' => $linkaceVersion])</p>
         <x-update-check/>
     </div>
+    <div class="card-body text-success">
+        <x-icon.info class="me-1"/> <strong>The LinkAce 2 Beta is available!</strong> Please help testing. <a href="https://github.com/Kovah/LinkAce/discussions/categories/announcements">Find out more</a>
+    </div>
     <div class="card-body text-danger">
         <x-icon.info class="me-1"/> Please note that the LinkAce Docker image will be renamed with the release of LinkAce 2! <a href="https://github.com/Kovah/LinkAce/issues/502">Read more</a>
     </div>
