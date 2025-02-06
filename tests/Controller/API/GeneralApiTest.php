271077f00837733047fe0c13eba4a3355dd14cb0
Kovah <mail@kovah.de>
2025-02-06 11:47:41 +0100

    Code style fix

--- tests/Controller/API/GeneralApiTest.php
+++ tests/Controller/API/GeneralApiTest.php
@@ -6,7 +6,7 @@ use Tests\Controller\API\ApiTestCase;
 
 class GeneralApiTest extends ApiTestCase
 {
-    public function test_version(): void
+    public function testVersion(): void
     {
         $this->getJsonAuthorized('api/version')->assertJson(['version' => 'v1']);
     }
