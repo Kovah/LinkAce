84272f9c16c5f40fec1907968337d5599f025017
Mateusz Hajder <6783135+mhajder@users.noreply.github.com>
2026-07-03 16:24:07 +0200

    fix(sso): read preferred_username claim during SSO login
    Prevents LinkAce from forcing a studly-cased full name on users when their identity provider supplies their actual username within the `preferred_username` OIDC claim.

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -33,7 +33,7 @@ class SocialiteController extends Controller
             }
 
             $user->update([
-                'name' => $authUser->getNickname() ?: Str::studly($authUser->getName()),
+                'name' => $authUser->getNickname() ?: (($authUser->getRaw() ?? [])['preferred_username'] ?? Str::studly($authUser->getName())),
                 'sso_id' => $authUser->id,
                 'sso_provider' => $provider,
                 'sso_token' => $authUser->token ?? null,
@@ -58,7 +58,7 @@ class SocialiteController extends Controller
                 'sso_id' => $authUser->getId(),
                 'sso_provider' => $provider,
             ], [
-                'name' => $authUser->getNickname() ?: Str::studly($authUser->getName()),
+                'name' => $authUser->getNickname() ?: (($authUser->getRaw() ?? [])['preferred_username'] ?? Str::studly($authUser->getName())),
                 'email' => $authUser->getEmail(),
                 'sso_id' => $authUser->getId(),
                 'sso_provider' => $provider,
